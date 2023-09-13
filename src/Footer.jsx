534186caa34833100366861502ead565403e9de6
himanshu verma <himanshuverma9170@gmail.com>
2023-09-13 23:00:06 +0530

    2

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,11 +6,11 @@ const Footer =()=>{
         <footer className="width-100% bg-light text-center">
             <p className="text-center"><b>&#169;</b> Copyright 2023 All right reserved.</p>
             <div className="footlink">
-              <a href="https://facebook.com" target="_blank" without rel="noreferrer" ><i className="fa-brands fa-square-facebook footerlink"></i></a>
-              <a href="https://web.whatsapp.com" target="_blank" without rel="noreferrer"  ><i className="fa-brands fa-square-whatsapp footerlink"></i></a>
-              <a href="https://instagram.com" target="_blank" without rel="noreferrer" ><i className="fa-brands fa-square-instagram footerlink"></i></a>
-              <a href="https://twitter.com"target="_blank" without rel="noreferrer"  ><i className="fa-brands fa-square-twitter footerlink"></i></a>
-              <a href="https://linkedin.com" target="_blank" without rel="noreferrer" ><i className="fa-brands fa-linkedin footerlink"></i></a>
+          <a href="https://facebook.com" target="_blank" without="true" rel="noreferrer" ><i className="fa-brands fa-square-facebook footerlink"></i></a>
+          <a href="https://web.whatsapp.com" target="_blank" without="true" rel="noreferrer"  ><i className="fa-brands fa-square-whatsapp footerlink"></i></a>
+          <a href="https://instagram.com" target="_blank" without="true" rel="noreferrer" ><i className="fa-brands fa-square-instagram footerlink"></i></a>
+          <a href="https://twitter.com" target="_blank" without="true" rel="noreferrer"  ><i className="fa-brands fa-square-twitter footerlink"></i></a>
+          <a href="https://linkedin.com" target="_blank" without="true" rel="noreferrer" ><i className="fa-brands fa-linkedin footerlink"></i></a>
             </div>
         </footer>
     </>
